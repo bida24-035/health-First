d08f4b99ef9877cd44ea7962ad00e08afde9a1b3
bida24-035 <bida24-035@thuto.bac.ac.bw>
2025-05-25 07:41:52 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+<script>
+  let currentFontSize = 16;
+  let isWhiteText = false;
+
+  function increaseFont() {
+    currentFontSize += 2;
+    document.body.style.fontSize = currentFontSize + "px";
+  }
+
+  function decreaseFont() {
+    if (currentFontSize > 10) {
+      currentFontSize -= 2;
+      document.body.style.fontSize = currentFontSize + "px";
+    }
+  }
+
+  function toggleTextColor() {
+    isWhiteText = !isWhiteText;
+    document.body.style.color = isWhiteText ? "white" : "black";
+  }
+</script>
